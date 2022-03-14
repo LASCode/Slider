912156d6090a29340f2e7ee14613ebf6d39979aa
LASCode <geimixa123@gmail.com>
2022-03-14 19:17:22 +0300

    create base Presenter

---
+++ src/Components/Presenter/Presenter.ts
@@ -0,0 +1,31 @@
+import { Model } from '../Model/Model';
+import { View } from '../View/View';
+import { sliderState } from '../Types/state';
+
+class Presenter {
+  rootNode: HTMLElement;
+  View: any;
+  Model: any;
+
+  constructor(element: HTMLElement, state: sliderState) {
+    this.rootNode = element;
+    this.Model = new Model({
+      state: state,
+      callback: this.modelEventChecker,
+    });
+    this.View = new View({
+      rootNode: this.rootNode,
+      state: this.Model.getState(),
+      callback: this.viewEventChecker,
+    });
+  }
+  viewEventChecker() {
+
+  }
+  modelEventChecker() {
+
+  }
+}
+
+export { Presenter };
+
