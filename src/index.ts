eaf02452acf8a071a431cc2664b977ec9a7e9dcb
LASCode <geimixa123@gmail.com>
2022-03-14 19:22:31 +0300

    update entry file. create jqSlider class

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,32 @@
-import './index.scss'
+import $ from 'jquery';
+import './index.scss';
+import { Presenter } from './Components/Presenter/Presenter';
+import { sliderState } from './Components/Types/state';
+
+
+class jqSlider {
+  rootNode: HTMLElement;
+  state: sliderState;
+  Presenter: Presenter;
+
+  constructor(element: HTMLElement, settings: sliderState) {
+    this.rootNode = element;
+    this.state = settings;
+    this.Presenter = new Presenter(this.rootNode, this.state);
+  }
+}
+
+
+
+
+
+$(document).ready(() => {
+  $('.page__slider').each((index, element) => {
+    const test = new jqSlider(element, {
+      max: 1000,
+      min: 0,
+      from: 500,
+    });
+  });
+});
 
