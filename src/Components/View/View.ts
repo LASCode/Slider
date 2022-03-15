d212b09b3f22938e17e17fd09547557a88b83a4f
LASCode <geimixa123@gmail.com>
2022-03-15 07:29:31 +0300

    add createSliderNode method (view layer)

--- src/Components/View/View.ts
+++ src/Components/View/View.ts
@@ -3,6 +3,7 @@ import { viewProps } from '../Types/props';
 
 class View {
   rootNode: HTMLElement;
+  sliderNode: HTMLElement;
   state: object;
   callback: Function;
 
@@ -10,6 +11,14 @@ class View {
     this.rootNode = props.rootNode;
     this.state = props.state;
     this.callback = props.callback;
+    this.sliderNode = this.createSliderNode();
+  }
+
+  createSliderNode() {
+    const element = document.createElement('div');
+    element.classList.add('jq-slider');
+    this.rootNode.appendChild(element);
+    return element;
   }
 }
 
