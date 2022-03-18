597779d344e7798bc3219ba17924f64499a014a6
LASCode <geimixa123@gmail.com>
2022-03-18 19:23:24 +0300

    update Presenter. Rename and update callback func;

--- src/Components/Presenter/Presenter.ts
+++ src/Components/Presenter/Presenter.ts
@@ -1,6 +1,8 @@
 import { Model } from '../Model/Model';
 import { View } from '../View/View';
 import { sliderState } from '../../Types/state';
+import {layoutEvent, viewEvent} from '../../Types/event';
+
 
 class Presenter {
   rootNode: HTMLElement;
@@ -11,19 +13,28 @@ class Presenter {
     this.rootNode = element;
     this.Model = new Model({
       state: state,
-      callback: this.modelEventChecker,
+      callback: this.modelEventHandler.bind(this),
     });
     this.View = new View({
       rootNode: this.rootNode,
       state: this.Model.getState(),
-      callback: this.viewEventChecker,
+      callback: this.viewEventHandler.bind(this),
     });
   }
-  viewEventChecker() {
-
+  viewEventHandler(event: viewEvent) {
+    this.Model.testData(this.pixelToValidNumber(event));
   }
-  modelEventChecker() {
-
+  modelEventHandler(state: sliderState) {
+    this.updateView(state);
+  }
+  pixelToValidNumber(action: viewEvent): viewEvent {
+    const copyAction = { ...action };
+    copyAction.value.x = (action.value.x - this.View.getOfSet()) / (this.View.getSize() / 100);
+    copyAction.value.y = (action.value.y - this.View.getOfSet()) / (this.View.getSize() / 100);
+    return copyAction;
+  }
+  updateView(state: sliderState) {
+    this.View.updateComponents(state);
   }
 }
 
