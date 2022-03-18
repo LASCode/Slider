a406cec784e2138de2592611f7fd2dfc34bc8d15
LASCode <geimixa123@gmail.com>
2022-03-18 19:36:11 +0300

    global refactoring of view class. Add subView components in view.

--- src/Components/View/View.ts
+++ src/Components/View/View.ts
@@ -1,17 +1,23 @@
 import { viewProps } from '../../Types/props';
+import { Line } from './subView/Line/Line';
+import { viewEvent } from '../../Types/event';
+import { Handle } from './subView/Handle/Handle';
+import { sliderState } from '../../Types/state';
 
 
 class View {
   rootNode: HTMLElement;
   sliderNode: HTMLElement;
-  state: object;
   callback: Function;
+  components!: Array<Line | Handle>
 
   constructor(props: viewProps) {
     this.rootNode = props.rootNode;
-    this.state = props.state;
     this.callback = props.callback;
+    this.components = [];
     this.sliderNode = this.createSliderNode();
+    this.createSubViewComponents();
+    this.setCallbackToSubVIewComponents();
   }
 
   createSliderNode() {
@@ -19,6 +25,25 @@ class View {
     element.classList.add('jq-slider');
     this.rootNode.appendChild(element);
     return element;
+  }
+  createSubViewComponents() {
+    this.components.push(new Line(this.sliderNode));
+    this.components.push(new Handle(this.sliderNode));
+  }
+  subViewEventListener(action: viewEvent) {
+    this.callback(action);
+  }
+  setCallbackToSubVIewComponents() {
+    this.components.forEach((el) => el.setCallback(this.subViewEventListener.bind(this)));
+  }
+  updateComponents(state: sliderState) {
+    this.components.forEach((component) => component.update(state));
+  }
+  getSize() {
+    return this.rootNode.clientWidth;
+  }
+  getOfSet() {
+    return this.rootNode.offsetLeft;
   }
 }
 
