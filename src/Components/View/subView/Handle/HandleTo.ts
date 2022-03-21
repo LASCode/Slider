0be26602591a8a28a6587a65fcfa6d8f62064d2a
LASCode <geimixa123@gmail.com>
2022-03-21 18:58:09 +0300

    create HandleTo and HandleFrom classes

---
+++ src/Components/View/subView/Handle/HandleTo.ts
@@ -0,0 +1,87 @@
+import { subViewElement } from '../subViewElement';
+import { DefaultSubViewElement } from '../../../../Types/defaultSubViewElement';
+import { sliderState } from '../../../../Types/state';
+
+
+class HandleTo extends subViewElement implements DefaultSubViewElement {
+  sliderNode: HTMLElement
+  componentNode!: HTMLElement
+  target: string = 'handle'
+  type: string = 'to'
+
+  constructor(sliderNode: HTMLElement) {
+    super();
+    this.sliderNode = sliderNode;
+    this.createComponent();
+    this.setListeners();
+  }
+  createComponent() {
+    const element = document.createElement('div');
+    element.classList.add('jq-slider__handle');
+    this.sliderNode.appendChild(element);
+    this.componentNode = element;
+  }
+  destroyComponent() {
+    this.removeListeners();
+    this.componentNode.remove();
+  }
+  removeListeners() {
+    this.componentNode.removeEventListener('pointerdown', this.onClick);
+  }
+  setListeners() {
+    this.onClick = this.onClick.bind(this);
+    this.onMove = this.onMove.bind(this);
+    this.onDrop = this.onDrop.bind(this);
+    this.componentNode.addEventListener('pointerdown', this.onClick);
+  }
+  update(state: sliderState) {
+    this.componentNode.style.left = `${state.to}%`;
+    this.componentNode.style.top = '';
+  }
+
+  onClick(event: PointerEvent) {
+    event.stopPropagation();
+    event.stopPropagation();
+    this.sendAction({
+      target: this.target,
+      type: this.type,
+      event: 'click',
+      value: {
+        x: event.clientX,
+        y: event.clientY,
+      },
+    });
+    document.addEventListener('pointermove', this.onMove);
+    document.addEventListener('pointerup', this.onDrop);
+  }
+  onMove(event: PointerEvent) {
+    event.stopPropagation();
+    this.sendAction({
+      target: this.target,
+      type: this.type,
+      event: 'move',
+      value: {
+        x: event.clientX,
+        y: event.clientY,
+      },
+    });
+  }
+  onDrop(event: PointerEvent) {
+    event.stopPropagation();
+    event.stopPropagation();
+    this.sendAction({
+      target: this.target,
+      type: this.type,
+      event: 'drop',
+      value: {
+        x: event.clientX,
+        y: event.clientY,
+      },
+    });
+    document.removeEventListener('pointermove', this.onMove);
+    document.removeEventListener('pointerup', this.onDrop);
+  }
+}
+
+
+export { HandleTo };
