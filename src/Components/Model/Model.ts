957cd6341ffa5b685c73ea7a71e4ef0553e38975
LASCode <geimixa123@gmail.com>
2022-03-21 19:13:43 +0300

    add handleMoved and tempValue properties, global refactoring testData method

--- src/Components/Model/Model.ts
+++ src/Components/Model/Model.ts
@@ -14,6 +14,8 @@ const initialState: sliderState = {
 class Model {
   state: sliderState;
   callback: Function;
+  handleMoved: string = ''
+  tempValue: number = 0;
 
   constructor(props: modelProps) {
     this.state = this.combineState(props.state);
@@ -25,15 +27,62 @@ class Model {
   combineState(state: sliderState): sliderState {
     return { ...initialState, ...state };
   }
+
+  borderValidator() {
+    if (this.state.from < this.state.min) { this.state.from = this.state.min; }
+    if (this.state.from > this.state.max) { this.state.from = this.state.max; }
+    if (this.state.to < this.state.min) { this.state.to = this.state.min; }
+    if (this.state.to > this.state.max) { this.state.to = this.state.max; }
+  }
   testData(action: viewEvent) {
-    if (this.state.type === 'horizontal'){
-      this.state.from = action.value.x;
+    if (action.target === 'handle') {
+      if (action.event === 'click') {
+        if (action.type === 'from') {
+          this.tempValue = this.state.to;
+        }
+        if (action.type === 'to') {
+          this.tempValue = this.state.from;
+        }
+      }
+      if (action.event === 'move') {
+        if (action.type === 'from') {
+          if (action.value.x > this.state.to) {
+            this.handleMoved = 'from';
+            this.state.from = this.tempValue;
+            this.state.to = action.value.x;
+          } else
+          if (action.value.x > this.state.from && this.handleMoved === 'from') {
+            this.state.from = this.tempValue;
+            this.state.to = action.value.x;
+          } else {
+            this.handleMoved = '';
+            this.state.from = this.tempValue;
+            this.state.from = action.value.x;
+          }
+        }
+        if (action.type === 'to') {
+          if (action.value.x < this.state.from) {
+            this.handleMoved = 'to';
+            this.state.to = this.tempValue;
+            this.state.from = action.value.x;
+          } else
+          if (action.value.x < this.state.to && this.handleMoved === 'to') {
+            this.state.to = this.tempValue;
+            this.state.from = action.value.x;
+          } else {
+            this.handleMoved = '';
+            this.state.from = this.tempValue;
+            this.state.to = action.value.x;
+          }
+        }
+      }
+      if (action.event === 'drop') {
+        this.handleMoved = '';
+        this.tempValue = 0;
+      }
     }
-    if (this.state.type === 'vertical'){
-      this.state.from = action.value.y;
-    }
-    if (action.value.x < 0) {this.state.from = 0}
-    if (action.value.x >= 100) {this.state.from = 100}
+
+    this.borderValidator();
     this.callback(this.state);
   }
 }
