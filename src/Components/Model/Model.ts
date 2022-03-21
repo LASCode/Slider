88b0c6ad4fb68b869b962164818dbef77e9b874d
LASCode <geimixa123@gmail.com>
2022-03-21 23:10:32 +0300

    Global refactoring of event handling

--- src/Components/Model/Model.ts
+++ src/Components/Model/Model.ts
@@ -8,7 +8,7 @@ const initialState: sliderState = {
   from: 50,
   to: 0,
   isRange: false,
-  type: 'horizontal',
+  horizontal: true,
 };
 
 class Model {
@@ -35,6 +35,8 @@ class Model {
     if (this.state.to > this.state.max) { this.state.to = this.state.max; }
   }
   testData(action: viewEvent) {
+    console.log(action);
+    const coordsName = this.state.horizontal ? 'x' : 'y';
     if (action.target === 'handle') {
       if (action.event === 'click') {
         if (action.type === 'from') {
@@ -46,33 +48,39 @@ class Model {
       }
       if (action.event === 'move') {
         if (action.type === 'from') {
-          if (action.value.x > this.state.to) {
-            this.handleMoved = 'from';
-            this.state.from = this.tempValue;
-            this.state.to = action.value.x;
-          } else
-          if (action.value.x > this.state.from && this.handleMoved === 'from') {
-            this.state.from = this.tempValue;
-            this.state.to = action.value.x;
+          if (this.state.isRange) {
+            if (action.value[coordsName] > this.state.to) {
+              this.handleMoved = 'from';
+              this.state.from = this.tempValue;
+              this.state.to = action.value[coordsName];
+            } else
+            if (action.value[coordsName] > this.state.from && this.handleMoved === 'from') {
+              this.state.from = this.tempValue;
+              this.state.to = action.value[coordsName];
+            } else {
+              this.handleMoved = '';
+              this.state.to = this.tempValue;
+              this.state.from = action.value[coordsName];
+            }
           } else {
             this.handleMoved = '';
             this.state.from = this.tempValue;
-            this.state.from = action.value.x;
+            this.state.from = action.value[coordsName];
           }
         }
         if (action.type === 'to') {
-          if (action.value.x < this.state.from) {
+          if (action.value[coordsName] < this.state.from) {
             this.handleMoved = 'to';
             this.state.to = this.tempValue;
-            this.state.from = action.value.x;
+            this.state.from = action.value[coordsName];
           } else
-          if (action.value.x < this.state.to && this.handleMoved === 'to') {
+          if (action.value[coordsName] < this.state.to && this.handleMoved === 'to') {
             this.state.to = this.tempValue;
-            this.state.from = action.value.x;
+            this.state.from = action.value[coordsName];
           } else {
             this.handleMoved = '';
             this.state.from = this.tempValue;
-            this.state.to = action.value.x;
+            this.state.to = action.value[coordsName];
           }
         }
       }
@@ -81,6 +89,33 @@ class Model {
         this.tempValue = 0;
       }
     }
+    if (action.target === 'line') {
+      if (action.event === 'click') {
+        if (this.state.isRange) {
+          if (action.value[coordsName] < this.state.from) {
+            this.state.from = action.value[coordsName];
+          } else {
+            this.state.to = action.value[coordsName];
+          }
+        } else {
+          this.state.from = action.value[coordsName];
+        }
+      }
+    }
+    if (action.target === 'range') {
+      if (action.event === 'click') {
+        if (this.state.isRange) {
+          if (((this.state.to - this.state.from) / 2 + this.state.from) > action.value[coordsName]) {
+            this.state.from = action.value[coordsName];
+          } else {
+            this.state.to = action.value[coordsName];
+          }
+        } else {
+          this.state.from = action.value[coordsName];
+        }
+      }
+    }
+
 
     this.borderValidator();
     this.callback(this.state);
