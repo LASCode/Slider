f8c448eaac3ba0803c945913baa619141ff84322
LASCode <geimixa123@gmail.com>
2022-04-12 05:25:25 +0300

    global refactoring of event types

--- src/Types/event.ts
+++ src/Types/event.ts
@@ -1,4 +1,17 @@
 
+
+
+interface resizeViewEvent {
+  type: 'resizeViewEvent',
+  data: {
+    width: number,
+    height: number,
+    offSetX: number,
+    offSetY: number,
+  }
+}
+type viewEventType = subViewEvent | resizeViewEvent
+
 interface viewEvent {
   target: string,
   type: string,
@@ -6,4 +19,34 @@ interface viewEvent {
   value: {x: number, y: number}
 }
 
-export { viewEvent };
+
+interface handleEvent {
+  target: 'handle',
+  type: 'from' | 'to',
+  event: 'click' | 'move' | 'drop',
+  value: {x: number, y: number}
+}
+interface lineEvent {
+  target: 'line',
+  type: string,
+  event: 'click' | 'move' | 'drop',
+  value: {x: number, y: number}
+}
+interface scaleEvent {
+  target: 'scale',
+  type: string,
+  event: 'click' | 'move' | 'drop',
+  value: {x: number, y: number},
+}
+interface rangeEvent {
+  target: 'range',
+  type: string,
+  event: 'click' | 'move' | 'drop',
+  value: {x: number, y: number}
+}
+interface subViewEvent {
+  type: 'subViewEvent',
+  data: handleEvent | lineEvent | rangeEvent | scaleEvent
+}
+
+export { viewEvent, viewEventType, subViewEvent, handleEvent, lineEvent, rangeEvent, scaleEvent };
