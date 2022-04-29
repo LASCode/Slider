68c721bac2df826cd12a789b272fba7451976dab
LASCode <geimixa123@gmail.com>
2022-04-29 15:42:14 +0300

    add customClass and customId properties to SliderState

--- src/Types/state.ts
+++ src/Types/state.ts
@@ -1,57 +1,54 @@
-interface sliderState {
-  max: number
-  min: number
-  step: number
-  size: {height: number, width: number, offSetX: number, offSetY: number}
-  from: {px: number, percent: number, total: number, isHold: boolean, isAbstractHold: boolean}
-  to: {px: number, percent: number, total: number, isHold: boolean, isAbstractHold: boolean}
-  isRange?: boolean
-  horizontal?: boolean
-  scaleStep: number
-  scaleStepsArr: Array<number>
-}
-
-interface userOptions {
-  max?: number,
-  min?: number,
-  from?: number,
-  to?: number,
-  step?: number,
-  isRange?: boolean,
-  horizontal?: boolean,
-  scaleStep?: number,
-  onChangeFunction?: Function,
-  changeValueFunction?: Function;
-}
 interface SliderState {
   max: number,
   min: number,
   from: number,
   to: number,
   step: number,
+  scaleStep: number,
+  customId: string,
+  customClass: string,
+  tips: boolean,
   isRange: boolean,
   horizontal: boolean,
-  scaleStep: number,
+  invert: boolean,
+  handleSplit: boolean,
   onChangeFunction: Function,
   changeValueFunction: Function;
 }
+
 type SliderStateModified = Partial<SliderState>
 type StateProperties = keyof SliderState
-interface viewSliderState {
-  max: number
-  min: number
-  horizontal: boolean
-  isRange: boolean
-  from: {px: number, percent: number, total: number, movingNow: boolean, pressedNow: boolean}
-  to: {px: number, percent: number, total: number, movingNow: boolean, pressedNow: boolean}
-  scaleItemsArray: number[]
+
+interface viewHandleState {
+  px: number,
+  percent: number,
+  total: number,
+  movingNow: boolean,
+  pressedNow: boolean,
+  pressedLast: boolean,
 }
-interface viewTempData {
+interface viewSliderState {
+  max: number,
+  min: number,
+  horizontal: boolean,
+  isRange: boolean,
+  invert: boolean,
+  tips: boolean,
+  handleSplit: boolean,
+  from: viewHandleState,
+  to: viewHandleState,
+  scaleItemsArray: number[],
+  customClass: string,
+  customId: string,
+}
+
+interface presenterTempData {
+  handlePressedLast: string | null,
   handleSelected: string | null,
   handleMovingNow: string | null,
   handlePressedNow: string | null,
   handleValue: number,
 }
 
-export { SliderState, SliderStateModified, StateProperties, viewSliderState, viewTempData, userOptions };
+export {SliderState, SliderStateModified, StateProperties, viewSliderState, presenterTempData};
 
