9beccbc8308ee90e82ee50110c55020e64195764
LASCode <geimixa123@gmail.com>
2022-04-29 15:37:50 +0300

    global refactoring :"

--- src/Components/Model/Model.ts
+++ src/Components/Model/Model.ts
@@ -1,35 +1,41 @@
 import {
   SliderState,
   SliderStateModified,
-  userOptions,
 } from '../../Types/state';
-import { toFixed2 } from '../../utils/utils';
+import {
+  getFixedValueWithStep,
+  getNumAfterComma,
+  checkStep,
+} from '../../utils/utils';
 
 
-const checkStep = (value: number, step: number) => {
-  const stepCopy = (step <= 0 ? 1 : step);
-  return +(Math.round(value / stepCopy) * stepCopy);
-};
+
 const defaultState: SliderState = {
   max: 100,
   min: 0,
   from: 0,
   to: 0,
   step: 0,
+  scaleStep: 0,
   isRange: false,
   horizontal: true,
-  scaleStep: 0,
+  tips: true,
+  handleSplit: true,
+  invert: false,
+  customId: '',
+  customClass: '',
   onChangeFunction: () => {},
   changeValueFunction: () => {},
 };
 
-
 class Model {
+  oldState: SliderState;
   state: SliderState;
   callback: Function;
 
-  constructor(props: {state: userOptions, callback: Function}) {
+  constructor(props: {state: SliderStateModified, callback: Function}) {
     this.state = { ...defaultState, ...props.state };
+    this.oldState = this.state;
     this.callback = props.callback;
     this.validateState();
   }
@@ -37,292 +43,42 @@ class Model {
     return this.state;
   }
   setState(properties: SliderStateModified) {
+    this.oldState = this.state;
     this.state = { ...this.state, ...properties };
+    this.validateTypes();
     this.validateState();
-    this.state.onChangeFunction();
+    this.state.onChangeFunction(this.state);
     this.callback(this.state);
   }
   validateState() {
-    const { max, min, from, to, step } = this.state;
+    const { max, min, from, to, step, isRange, scaleStep } = this.state;
 
-    if (min > max) { this.state.max = min; }
+    if (min > max) { this.state.max = this.state.min; }
 
-    this.state.from = toFixed2(from);
-    this.state.to = toFixed2(to);
+    if (scaleStep < 0) { this.state.scaleStep = 0; }
+    if (step < 0) { this.state.step = 0; }
+    if (getNumAfterComma(this.state.step) > 5) { this.state.step = 0.00001; }
 
-    this.state.from = checkStep(from, step);
-    this.state.to = checkStep(to, step);
+    this.state.from = checkStep(this.state.from, this.state.step);
+    this.state.to = checkStep(this.state.to, this.state.step);
 
+    this.state.from = getFixedValueWithStep(this.state.from, this.state.step);
+    this.state.to = getFixedValueWithStep(this.state.to, this.state.step);
 
-    if (from < min) { this.state.from = min; }
-    if (from > max) { this.state.from = max; }
-    if (to < min) { this.state.to = min; }
-    if (to > max) { this.state.to = max; }
-
-
+    if (this.state.max === min) { this.state.max = this.state.max + 1; }
+    if (this.state.from < min) { this.state.from = this.state.min; }
+    if (this.state.from > this.state.max) { this.state.from = this.state.max; }
+    if (isRange && this.state.to < this.state.from) { this.state.to = this.state.from; this.state.from = this.state.to; }
+    if (this.state.to < this.state.min) { this.state.to = this.state.max; }
+    if (this.state.to > this.state.max) { this.state.to = this.state.max; }
   }
-
-
-
-
-
-
-
-
-  // getState(keys: StateProperties[] | null = null): SliderStateModified {
-  //   let result: SliderStateModified = {};
-  //   if (keys) {
-  //     result = Object.fromEntries(
-  //       Object.entries(this.state).filter(([key]) => keys.includes(key as StateProperties)),
-  //     );
-  //   } else {
-  //     result = this.state;
-  //   }
-  //   return result;
-  // }
-  // getState() {
-  //   return this.state;
-  // }
-  // combineState(state: sliderState): sliderState {
-  //   return { ...initialState, ...state };
-  // }
-  //
-  // convertPixelToValue(px: number): number {
-  //   const { horizontal, size, max, min } = this.state;
-  //   const offSet = horizontal ? size.offSetX : size.offSetY;
-  //   const width = horizontal ? size.width : size.height;
-  //   return (px - offSet) / (width / (max - min)) + min;
-  // }
-  // convertValueToPercent(value: number): number {
-  //   const { horizontal, size, max, min } = this.state;
-  //   return (100 / (max - min)) * (value - min);
-  // }
-  // correctValueToStep(value: number, step: number): number {
-  //   const stepCopy = (step <= 0 ? 0.01 : step);
-  //   return +(Math.round(value / stepCopy) * stepCopy);
-  // }
-  //
-  // lineValidator(action: lineEvent) {
-  //   const modAction = this.normalizeActionValue(action);
-  //   const { target, type, event, value } = modAction;
-  //   const typeOfAxleCoords = this.state.horizontal ? 'x' : 'y';
-  //   if (event === 'click') {
-  //     if (this.state.isRange) {
-  //       if (value[typeOfAxleCoords] < this.state.from.total) {
-  //         this.state.from.total = action.value[typeOfAxleCoords];
-  //       } else {
-  //         this.state.to.total = action.value[typeOfAxleCoords];
-  //       }
-  //     } else {
-  //       this.state.from.total = action.value[typeOfAxleCoords];
-  //     }
-  //   }
-  // }
-  // rangeValidator(action: rangeEvent) {
-  //   const modAction = this.normalizeActionValue(action);
-  //   const { target, type, event, value } = modAction;
-  //   const typeOfAxleCoords = this.state.horizontal ? 'x' : 'y';
-  //   if (event === 'click') {
-  //     if (this.state.isRange) {
-  //       if (((this.state.to.total - this.state.from.total) / 2 + this.state.from.total) > value[typeOfAxleCoords]) {
-  //         this.state.from.total = value[typeOfAxleCoords];
-  //       } else {
-  //         this.state.to.total = value[typeOfAxleCoords];
-  //       }
-  //     } else {
-  //       this.state.from.total = value[typeOfAxleCoords];
-  //     }
-  //   }
-  // }
-  // scaleValidator(action: scaleEvent) {
-  //   const copyAction = this.normalizeActionValue(action);
-  //   const { target, type, event, value } = copyAction;
-  //   const typeOfAxleCoords = this.state.horizontal ? 'x' : 'y';
-  //   const correctStepValue = this.correctValueToStep(value[typeOfAxleCoords], this.state.scaleStep);
-  //   console.log(copyAction)
-  //
-  //   if (event === 'click') {
-  //     if (this.state.isRange) {
-  //       const fromOffSet = Math.abs(this.state.from.total - correctStepValue);
-  //       const toOffSet = Math.abs(this.state.to.total - correctStepValue);
-  //       if (fromOffSet > toOffSet) { this.state.to.total = correctStepValue; }
-  //       if (fromOffSet < toOffSet) { this.state.from.total = correctStepValue; }
-  //       if (fromOffSet === toOffSet) { this.state.from.total = correctStepValue; }
-  //     } else {
-  //       this.state.from.total = correctStepValue;
-  //     }
-  //   }
-  // }
-  // handleValidator(action: handleEvent) {
-  //   const { target, type, event, value } = action;
-  //   value.x = (value.x - this.state.size.offSetX) / ((this.state.horizontal ? this.state.size.width : this.state.size.height) / (this.state.max - this.state.min)) + this.state.min;
-  //   value.y = (value.y - this.state.size.offSetY) / ((this.state.horizontal ? this.state.size.width : this.state.size.height) / (this.state.max - this.state.min)) + this.state.min;
-  //
-  //   const typeOfAxleCoords = this.state.horizontal ? 'x' : 'y';
-  //   const oppositeType = (type === 'from' ? 'to' : 'from');
-  //   const isFrom = (type === 'from');
-  //   if (event === 'click') {
-  //     this.state[type].isHold = true;
-  //     this.state[type].isAbstractHold = true;
-  //     this.tempValue = this.state[oppositeType].total;
-  //   }
-  //   if (event === 'move') {
-  //     if (this.state.isRange) {
-  //       if ((isFrom ? value[typeOfAxleCoords] > this.state.to.total : value[typeOfAxleCoords] < this.state.from.total)) {
-  //         this.state[oppositeType].isAbstractHold = true;
-  //         this.state[type].isAbstractHold = false;
-  //         this.handleMoved = type;
-  //         this.state[type].total = this.tempValue;
-  //         this.state[oppositeType].total = value[typeOfAxleCoords];
-  //       } else
-  //       if ((isFrom ? value[typeOfAxleCoords] > this.state.from.total : value[typeOfAxleCoords] < this.state.to.total) && this.handleMoved === type) {
-  //         this.state[type].isAbstractHold = false;
-  //         this.state[oppositeType].isAbstractHold = true;
-  //         this.state[type].total = this.tempValue;
-  //         this.state[oppositeType].total = value[typeOfAxleCoords];
-  //       } else {
-  //         this.state[type].isHold = true;
-  //         this.state[type].isAbstractHold = true;
-  //         this.state[oppositeType].isHold = false;
-  //         this.state[oppositeType].isAbstractHold = false;
-  //         this.handleMoved = '';
-  //         this.state[oppositeType].total = this.tempValue;
-  //         this.state[type].total = value[typeOfAxleCoords];
-  //       }
-  //     } else {
-  //       // this.state[type].isHold = true;
-  //       // this.state[type].isAbstractHold = true;
-  //       this.handleMoved = '';
-  //       this.state[type].total = this.tempValue;
-  //       this.state[type].total = value[typeOfAxleCoords];
-  //     }
-  //
-  //
-  //     if (type === 'from' && false) {
-  //       if (this.state.isRange) {
-  //         if (value[typeOfAxleCoords] > this.state.to.total) {
-  //           this.handleMoved = 'from';
-  //           this.state.from.total = this.tempValue;
-  //           this.state.to.total = value[typeOfAxleCoords];
-  //         } else
-  //         if (value[typeOfAxleCoords] > this.state.from.total && this.handleMoved === 'from') {
-  //           this.state.from.total = this.tempValue;
-  //           this.state.to.total = value[typeOfAxleCoords];
-  //         } else {
-  //           this.handleMoved = '';
-  //           this.state.to.total = this.tempValue;
-  //           this.state.from.total = value[typeOfAxleCoords];
-  //         }
-  //       } else {
-  //         this.handleMoved = '';
-  //         this.state.from.total = this.tempValue;
-  //         this.state.from.total = value[typeOfAxleCoords];
-  //       }
-  //     }
-  //     if (type === 'to' && false) {
-  //       if (value[typeOfAxleCoords] < this.state.from.total) {
-  //         this.handleMoved = 'to';
-  //         this.state.to.total = this.tempValue;
-  //         this.state.from.total = value[typeOfAxleCoords];
-  //       } else
-  //       if (value[typeOfAxleCoords] < this.state.to.total && this.handleMoved === 'to') {
-  //         this.state.to.total = this.tempValue;
-  //         this.state.from.total = value[typeOfAxleCoords];
-  //       } else {
-  //         this.handleMoved = '';
-  //         this.state.from.total = this.tempValue;
-  //         this.state.to.total = value[typeOfAxleCoords];
-  //       }
-  //     }
-  //   }
-  //   if (event === 'drop') {
-  //     this.state[oppositeType].isHold = false;
-  //     this.state[oppositeType].isAbstractHold = false;
-  //     this.state[type].isAbstractHold = false;
-  //     this.state[type].isHold = false;
-  //     this.handleMoved = '';
-  //     this.tempValue = 0;
-  //   }
-  // }
-  //
-  // borderValidator() {
-  //   if (this.state.from.total < this.state.min) { this.state.from.total = this.state.min; }
-  //   if (this.state.from.total > this.state.max) { this.state.from.total = this.state.max; }
-  //   if (this.state.to.total < this.state.min) { this.state.to.total = this.state.min; }
-  //   if (this.state.to.total > this.state.max) { this.state.to.total = this.state.max; }
-  // }
-  // totalValueValidator() {
-  //   const b1 = (v1:number)=>{
-  //     return 100 / (this.state.max - this.state.min) * (v1 - this.state.min)
-  //   };
-  //   // this.state.from.percent = (this.state.from.total / b) * 100;
-  //   // this.state.from.percent = this.state.from.total / ((this.state.max - this.state.min) / 100) + offSetPercent;
-  //   this.state.from.percent = b1(this.state.from.total);
-  //   this.state.to.percent = b1(this.state.to.total);
-  //   // this.state.to.percent = this.state.to.total / ((this.state.max - this.state.min) / 100) + offSetPercent;
-  //   this.state.from.px = ((this.state.horizontal?this.state.size.width:this.state.size.height) / 100) * this.state.from.percent;
-  //   this.state.to.px = ((this.state.horizontal?this.state.size.width:this.state.size.height) / 100) * this.state.to.percent;
-  //   // this.state.from = {
-  //   //   ...this.state.from,
-  //   //   percent: this.state.from.total / ((this.state.max - this.state.min) / 100),
-  //   //   px: ((this.state.horizontal?this.state.size.width:this.state.size.height)/100),
-  //   // };
-  //   // this.state.to = {
-  //   //   ...this.state.to,
-  //   //   percent: this.state.to.total / ((this.state.max - this.state.min) / 100),
-  //   //   px: (this.state.horizontal?this.state.size.width:this.state.size.height)/100,
-  //   // };
-  // }
-  // stepValidator() {
-  //   function checkStep(value: number, step: number): number {
-  //     const stepCopy = (step <= 0 ? 0.01 : step);
-  //     return +(Math.round(value / stepCopy) * stepCopy);
-  //   }
-  //   this.state.from.total = checkStep(this.state.from.total, this.state.step);
-  //   this.state.to.total = checkStep(this.state.to.total, this.state.step);
-  // }
-  // toFixedValidator() {
-  //   this.state = {
-  //     ...this.state,
-  //     from: {
-  //       ...this.state.from,
-  //       total: Math.floor(this.state.from.total * 100) / 100,
-  //       px: Math.floor(this.state.from.px * 100) / 100,
-  //       percent: Math.floor(this.state.from.percent * 100) / 100,
-  //     },
-  //     to: {
-  //       ...this.state.to,
-  //       total: Math.floor(this.state.to.total * 100) / 100,
-  //       px: Math.floor(this.state.to.px * 100) / 100,
-  //       percent: Math.floor(this.state.to.percent * 100) / 100,
-  //     }
-  //   }
-  // }
-  //
-  // normalizeActionValue(action: lineEvent | handleEvent | rangeEvent | scaleEvent) {
-  //   const copyAction = { ...action };
-  //   copyAction.value.x = this.convertPixelToValue(copyAction.value.x);
-  //   copyAction.value.y = this.convertPixelToValue(copyAction.value.y);
-  //   return copyAction;
-  // }
-  //
-  // testData(action: viewEventType) {
-  //   if (action.type === 'resizeViewEvent') {
-  //     this.state.size = { ...this.state.size, ...action.data };
-  //     this.callback(this.state);
-  //   }
-  //   if (action.type === 'subViewEvent') {
-  //     if (action.data.target === 'handle') { this.handleValidator(action.data); }
-  //     if (action.data.target === 'line') { this.lineValidator(action.data); }
-  //     if (action.data.target === 'range') { this.rangeValidator(action.data); }
-  //     if (action.data.target === 'scale') { this.scaleValidator(action.data); }
-  //   }
-  //   this.stepValidator();
-  //   this.borderValidator();
-  //   this.totalValueValidator();
-  //   this.toFixedValidator();
-  //   this.callback(this.state);
-  // }
+  validateTypes() {
+    const { max, min, from, to, step, isRange } = this.state;
+    if (typeof max !== 'number') { this.state.max = this.oldState.max; }
+    if (typeof min !== 'number') { this.state.min = this.oldState.min; }
+    if (typeof from !== 'number') { this.state.from = this.oldState.from; }
+    if (typeof to !== 'number') { this.state.to = this.oldState.to; }
+  }
 }
 
 export { Model };
--- src/Components/Presenter/Presenter.ts
+++ src/Components/Presenter/Presenter.ts
@@ -3,26 +3,26 @@ import { View } from '../View/View';
 import {
   SliderState,
   SliderStateModified,
-  userOptions,
   viewSliderState,
-  viewTempData,
+  presenterTempData,
 } from '../../Types/state';
-import { subViewEvent } from '../../Types/ViewEventTypes';
-import {getScaleItemsArray, invertValue, pixelToValue, toFixed2, valueToPercent, valueToPixel} from '../../utils/utils';
+import { subViewEvent, subViewValue } from '../../Types/ViewEventTypes';
+import { getScaleItemsArray, invertValue, pixelToValue, valueToPercent, valueToPixel } from '../../utils/utils';
 
 
 class Presenter {
   rootNode: HTMLElement;
   View: View;
   Model: Model;
-  private tempData: viewTempData = {
+  private tempData: presenterTempData = {
+    handlePressedLast: null,
     handleMovingNow: null,
     handlePressedNow: null,
     handleSelected: null,
     handleValue: 0,
   }
 
-  constructor(element: HTMLElement, state: userOptions) {
+  constructor(element: HTMLElement, state: SliderStateModified) {
     this.rootNode = element;
     this.Model = new Model({
       state: state,
@@ -33,18 +33,17 @@ class Presenter {
       callback: this.viewEventHandler.bind(this),
     });
     this.View.update(this.convertStateToViewState(this.Model.getState()));
-    // console.log(this.convertStateToViewState(this.Model.getState()));
   }
 
   getCoordsWithOffSet(coords: {x: number, y: number}) {
-    const { offSetY, offSetX } = this.View.getOffSet();
+    const { clientOffSetX, clientOffSetY } = this.View.getOffSet();
     return ({
-      x: coords.x - offSetX,
-      y: coords.y - offSetY,
+      x: coords.x - clientOffSetX,
+      y: coords.y - clientOffSetY,
     });
   }
   handleValidate = (event: subViewEvent) => {
-    const { to, from, isRange } = this.Model.getState();
+    const { to, from, isRange, handleSplit } = this.Model.getState();
     const { action, type } = event;
     const { handleSelected } = this.tempData;
     const value = this.normalizeEventValue(event.value);
@@ -58,27 +57,39 @@ class Presenter {
     if (action === 'click') {
       this.tempData.handleValue = handleValue.opposite;
       this.tempData.handlePressedNow = type;
+      this.tempData.handlePressedLast = type;
     }
     if (action === 'move') {
       if (isRange) {
-        if ((isFrom ? correctValue > to : correctValue < from)) {
-          this.tempData.handleSelected = type;
-          this.tempData.handleMovingNow = handleTypeOpposite;
-          result[type] = this.tempData.handleValue;
-          result[handleTypeOpposite] = correctValue;
-        } else if ((isFrom ? correctValue > from : correctValue < to) && handleSelected === type) {
-          this.tempData.handleMovingNow = handleTypeOpposite;
-          result[type] = this.tempData.handleValue;
-          result[handleTypeOpposite] = correctValue;
-        } else {
-          this.tempData.handleMovingNow = type;
-          this.tempData.handleSelected = null;
-          result[handleTypeOpposite] = this.tempData.handleValue;
+        if (handleSplit) {
+          if ((isFrom ? correctValue > to : correctValue < from)) {
+            this.tempData.handleSelected = type;
+            this.tempData.handleMovingNow = handleTypeOpposite;
+            this.tempData.handlePressedLast = handleTypeOpposite;
+            result[type] = this.tempData.handleValue;
+            result[handleTypeOpposite] = correctValue;
+          } else if ((isFrom
+            ? correctValue > from
+            : correctValue < to)
+            && handleSelected === type) {
+            this.tempData.handleMovingNow = handleTypeOpposite;
+            result[type] = this.tempData.handleValue;
+            this.tempData.handlePressedLast = handleTypeOpposite;
+            result[handleTypeOpposite] = correctValue;
+          } else {
+            this.tempData.handlePressedLast = type;
+            this.tempData.handleMovingNow = type;
+            this.tempData.handleSelected = null;
+            result[handleTypeOpposite] = this.tempData.handleValue;
+            result[type] = correctValue;
+          }
+        } else if (isFrom ? correctValue < to : correctValue > from) {
           result[type] = correctValue;
+        } else if (isFrom ? correctValue > to : correctValue < from) {
+          result[type] = handleValue.opposite;
         }
       } else {
         this.tempData.handleSelected = null;
-        result[type] = this.tempData.handleValue;
         result[type] = correctValue;
       }
     }
@@ -88,6 +99,7 @@ class Presenter {
       this.tempData.handlePressedNow = null;
       this.tempData.handleValue = 0;
     }
+
     return result;
   };
   lineValidate(event: subViewEvent) {
@@ -124,6 +136,7 @@ class Presenter {
       case 'range': modelEvent = this.lineValidate(eventCopy); break;
       case 'scale': modelEvent = this.lineValidate(eventCopy); break;
       case 'handle': modelEvent = this.handleValidate(eventCopy); break;
+      case 'tip': modelEvent = this.handleValidate(eventCopy); break;
 
       default: break;
     }
@@ -139,43 +152,46 @@ class Presenter {
     this.Model.setState(event);
   }
   convertStateToViewState(state: SliderState): viewSliderState {
-    const { from, to, max, min, horizontal, isRange, scaleStep } = state;
+    const { from, to, max, min, horizontal, scaleStep } = state;
     const { height, width } = this.View.getSize();
     return ({
-      max,
-      min,
+      ...state,
       from: {
-        percent: toFixed2(valueToPercent(from, max, min)),
-        px: toFixed2(valueToPixel(from, horizontal ? width : height, max, min)),
-        total: toFixed2(from),
+        percent: valueToPercent(from, max, min),
+        px: valueToPixel(from, horizontal ? width : height, max, min),
+        total: from,
         movingNow: this.tempData.handleMovingNow === 'from',
         pressedNow: this.tempData.handlePressedNow === 'from',
+        pressedLast: this.tempData.handlePressedLast === 'from',
       },
       to: {
-        percent: toFixed2(valueToPercent(to, max, min)),
-        px: toFixed2(valueToPixel(to, horizontal ? width : height, max, min)),
-        total: toFixed2(to),
+        percent: valueToPercent(to, max, min),
+        px: valueToPixel(to, horizontal ? width : height, max, min),
+        total: to,
         movingNow: this.tempData.handleMovingNow === 'to',
         pressedNow: this.tempData.handlePressedNow === 'to',
+        pressedLast: this.tempData.handlePressedLast === 'to',
       },
       scaleItemsArray: getScaleItemsArray(max, min, scaleStep),
-      isRange,
-      horizontal,
     });
   }
-  normalizeEventValue(eventValue: {x: number, y: number, total: number | boolean}): number {
-    const { horizontal, min, max } = this.Model.getState();
+  normalizeEventValue(eventValue: subViewValue): number {
+    const { horizontal, min, max, invert } = this.Model.getState();
     const { height, width } = this.View.getSize();
-    const { total, y, x } = eventValue;
+    const { total, y, x} = eventValue;
     let result = 0;
     if (typeof total === 'number') {
       result = total;
     } else
     if (horizontal) {
-      result = pixelToValue(x, horizontal ? width : height, max, min);
+      result = invert
+        ? invertValue(max, min, pixelToValue(x, horizontal ? width : height, max, min))
+        : pixelToValue(x, horizontal ? width : height, max, min);
     } else
     if (!horizontal) {
-      result = pixelToValue(y, horizontal ? width : height, max, min);
+      result = invert
+        ? invertValue(max, min, pixelToValue(y, horizontal ? width : height, max, min))
+        : pixelToValue(y, horizontal ? width : height, max, min);
     }
     return result;
   }
