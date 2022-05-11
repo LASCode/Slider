088befdb0768d77b3551f4c98d0f77464f5aa0a3
LASCode <geimixa123@gmail.com>
2022-05-11 17:14:30 +0300

    create jest polyfills (pointerevent, domrect)

---
+++ jest/DOMRectPolyfill.ts
@@ -0,0 +1,20 @@
+
+if (!global.DOMRect) {
+  class DOMRect {
+    bottom: number = 0;
+    left: number = 0;
+    right: number = 0;
+    top: number = 0;
+    x: number = 0;
+    y: number= 0;
+    width: number = 0;
+    height: number = 0;
+    constructor(x: number, y: number, width: number, height: number) {
+      this.y = y;
+      this.x = x;
+      this.width = width;
+      this.height = height;
+    }
+  }
+  global.DOMRect = DOMRect as any;
+}
