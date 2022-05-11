c82da7fbbef29d20984cceb9c405fe457b77b4d5
LASCode <geimixa123@gmail.com>
2022-05-11 17:13:25 +0300

    create jest config file

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+
+const config = {
+  verbose: false,
+  testEnvironment: 'jsdom',
+  testEnvironmentOptions: {
+    url: 'https://lascode.space/',
+    html: '<html lang=\'en\'><head><title></title></head><body></body></html>',
+  },
+  setupFiles: [
+    '<rootDir>/jest/PointerEventPolyfill.ts', 
+    '<rootDir>/jest/DOMRectPolyfill.ts',
+  ],
+};
+
+module.exports = config;
