1f59c1edcbb7e57447df8acf8f5b0b1ae5409014
LASCode <geimixa123@gmail.com>
2022-05-15 14:11:53 +0300

    global refactoring

--- src/Components/Model/Model.ts
+++ src/Components/Model/Model.ts
@@ -1,41 +1,15 @@
-import {
-  SliderState,
-  SliderStateModified,
-} from '../../Types/state';
-import {
-  getFixedValueWithStep,
-  getNumAfterComma,
-  checkStep,
-} from '../../utils/utils';
+import { SliderState, SliderStateModified } from '../../Types/state';
+import { checkStep, getFixedValueWithStep } from '../../utils/utils';
+import { modelProps } from '../../Types/props';
 
-
-
-const defaultState: SliderState = {
-  max: 100,
-  min: 0,
-  from: 0,
-  to: 0,
-  step: 0,
-  scaleStep: 0,
-  isRange: false,
-  horizontal: true,
-  tips: true,
-  handleSplit: true,
-  invert: false,
-  customId: '',
-  customClass: '',
-  onChangeFunction: () => {},
-  changeValueFunction: () => {},
-};
 
 class Model {
-  oldState: SliderState;
   state: SliderState;
   callback: Function;
+  componentInstance: any;
 
-  constructor(props: {state: SliderStateModified, callback: Function}) {
-    this.state = { ...defaultState, ...props.state };
-    this.oldState = this.state;
+  constructor(props: modelProps) {
+    this.state = { ...this.getDefaultState(), ...props.state };
     this.callback = props.callback;
     this.validateState();
   }
@@ -43,41 +17,67 @@ class Model {
     return this.state;
   }
   setState(properties: SliderStateModified) {
-    this.oldState = this.state;
     this.state = { ...this.state, ...properties };
-    this.validateTypes();
     this.validateState();
     this.state.onChangeFunction(this.state);
     this.callback(this.state);
   }
+  getDefaultState(): SliderState {
+    return ({
+      max: 100,
+      min: 0,
+      from: 0,
+      to: 0,
+      step: 0,
+      scaleStep: 0,
+      isRange: false,
+      horizontal: true,
+      tips: true,
+      handleSplit: true,
+      invert: false,
+      customId: '',
+      customClass: '',
+      onChangeFunction: () => {},
+      changeValueFunction: () => {},
+    });
+  }
   validateState() {
     const { max, min, from, to, step, isRange, scaleStep } = this.state;
 
-    if (min > max) { this.state.max = this.state.min; }
+    if (this.state.min >= this.state.max) this.state.min = this.state.max - 1;
 
-    if (scaleStep < 0) { this.state.scaleStep = 0; }
-    if (step < 0) { this.state.step = 0; }
-    if (getNumAfterComma(this.state.step) > 5) { this.state.step = 0.00001; }
+    if (this.state.scaleStep < 0) { this.state.scaleStep = 0; }
 
-    this.state.from = checkStep(this.state.from, this.state.step);
-    this.state.to = checkStep(this.state.to, this.state.step);
+    if (this.state.step > this.state.max) { this.state.step = this.state.max }
+    if (this.state.step <= 0.00001) { this.state.step = 0.00001; }
 
-    this.state.from = getFixedValueWithStep(this.state.from, this.state.step);
-    this.state.to = getFixedValueWithStep(this.state.to, this.state.step);
+    this.state.from = checkStep({
+      value: this.state.from,
+      step: this.state.step,
+    });
+    this.state.to = checkStep({
+      value: this.state.to,
+      step: this.state.step,
+    });
 
-    if (this.state.max === min) { this.state.max = this.state.max + 1; }
-    if (this.state.from < min) { this.state.from = this.state.min; }
+    this.state.from = getFixedValueWithStep({
+      value: this.state.from,
+      step: this.state.step,
+    });
+    this.state.to = getFixedValueWithStep({
+      value: this.state.to,
+      step: this.state.step,
+    });
+
+
+    if (this.state.from < this.state.min) { this.state.from = this.state.min; }
     if (this.state.from > this.state.max) { this.state.from = this.state.max; }
-    if (isRange && this.state.to < this.state.from) { this.state.to = this.state.from; this.state.from = this.state.to; }
+    if (this.state.isRange && this.state.to < this.state.from) {
+      this.state.to = this.state.from;
+      this.state.from = this.state.to;
+    }
     if (this.state.to < this.state.min) { this.state.to = this.state.max; }
     if (this.state.to > this.state.max) { this.state.to = this.state.max; }
-  }
-  validateTypes() {
-    const { max, min, from, to, step, isRange } = this.state;
-    if (typeof max !== 'number') { this.state.max = this.oldState.max; }
-    if (typeof min !== 'number') { this.state.min = this.oldState.min; }
-    if (typeof from !== 'number') { this.state.from = this.oldState.from; }
-    if (typeof to !== 'number') { this.state.to = this.oldState.to; }
   }
 }
 
--- src/Components/Presenter/Presenter.ts
+++ src/Components/Presenter/Presenter.ts
@@ -6,8 +6,9 @@ import {
   viewSliderState,
   presenterTempData,
 } from '../../Types/state';
-import { subViewEvent, subViewValue } from '../../Types/ViewEventTypes';
+import { subViewEvent } from '../../Types/ViewEventTypes';
 import { getScaleItemsArray, invertValue, pixelToValue, valueToPercent, valueToPixel } from '../../utils/utils';
+import { presenterProps } from '../../Types/props';
 
 
 class Presenter {
@@ -22,10 +23,10 @@ class Presenter {
     handleValue: 0,
   }
 
-  constructor(element: HTMLElement, state: SliderStateModified) {
-    this.rootNode = element;
+  constructor(props: presenterProps) {
+    this.rootNode = props.rootNode;
     this.Model = new Model({
-      state: state,
+      state: props.initialState,
       callback: this.modelEventHandler.bind(this),
     });
     this.View = new View({
@@ -35,24 +36,29 @@ class Presenter {
     this.View.update(this.convertStateToViewState(this.Model.getState()));
   }
 
-  getCoordsWithOffSet(coords: {x: number, y: number}) {
-    const { clientOffSetX, clientOffSetY } = this.View.getOffSet();
-    return ({
-      x: coords.x - clientOffSetX,
-      y: coords.y - clientOffSetY,
-    });
-  }
-  handleValidate = (event: subViewEvent) => {
+
+  handleValidate(event: subViewEvent): SliderStateModified {
     const { to, from, isRange, handleSplit } = this.Model.getState();
     const { action, type } = event;
     const { handleSelected } = this.tempData;
-    const value = this.normalizeEventValue(event.value);
+    const value = this.getCorrectPositionFromEventValue(event);
+    const result: SliderStateModified = {};
+
+    if (value === false) return {};
 
     const isFrom = (type === 'from');
     const correctValue = value;
     const handleTypeOpposite = isFrom ? 'to' : 'from';
     const handleValue = { current: isFrom ? from : to, opposite: isFrom ? to : from };
-    const result: SliderStateModified = {};
+
+    const toLessValue = to < value;
+    const toLargerValue = to > value;
+    const fromLessValue = from < value;
+    const fromLargerValue = from > value;
+    const handlesHasSwitched = handleSelected === type;
+    const beyondHisPlace = (isFrom ? toLessValue : fromLargerValue);
+    const insideHisPlace = (isFrom ? fromLessValue : toLargerValue);
+
 
     if (action === 'click') {
       this.tempData.handleValue = handleValue.opposite;
@@ -60,37 +66,38 @@ class Presenter {
       this.tempData.handlePressedLast = type;
     }
     if (action === 'move') {
-      if (isRange) {
-        if (handleSplit) {
-          if ((isFrom ? correctValue > to : correctValue < from)) {
-            this.tempData.handleSelected = type;
-            this.tempData.handleMovingNow = handleTypeOpposite;
-            this.tempData.handlePressedLast = handleTypeOpposite;
-            result[type] = this.tempData.handleValue;
-            result[handleTypeOpposite] = correctValue;
-          } else if ((isFrom
-            ? correctValue > from
-            : correctValue < to)
-            && handleSelected === type) {
-            this.tempData.handleMovingNow = handleTypeOpposite;
-            result[type] = this.tempData.handleValue;
-            this.tempData.handlePressedLast = handleTypeOpposite;
-            result[handleTypeOpposite] = correctValue;
-          } else {
-            this.tempData.handlePressedLast = type;
-            this.tempData.handleMovingNow = type;
-            this.tempData.handleSelected = null;
-            result[handleTypeOpposite] = this.tempData.handleValue;
-            result[type] = correctValue;
-          }
-        } else if (isFrom ? correctValue < to : correctValue > from) {
-          result[type] = correctValue;
-        } else if (isFrom ? correctValue > to : correctValue < from) {
-          result[type] = handleValue.opposite;
-        }
-      } else {
+      if (isRange && handleSplit && beyondHisPlace) {
+        this.tempData.handleSelected = type;
+        this.tempData.handleMovingNow = handleTypeOpposite;
+        this.tempData.handlePressedLast = handleTypeOpposite;
+        result[type] = this.tempData.handleValue;
+        result[handleTypeOpposite] = correctValue;
+      } else
+      if (isRange && handleSplit && insideHisPlace && handlesHasSwitched) {
+        this.tempData.handleMovingNow = handleTypeOpposite;
+        this.tempData.handlePressedLast = handleTypeOpposite;
+        result[type] = this.tempData.handleValue;
+        result[handleTypeOpposite] = correctValue;
+      } else
+      if (isRange && handleSplit) {
+        this.tempData.handlePressedLast = type;
+        this.tempData.handleMovingNow = type;
+        this.tempData.handleSelected = null;
+        result[handleTypeOpposite] = this.tempData.handleValue;
+        result[type] = correctValue;
+      } else
+      if (isRange && !handleSplit && (isFrom ? toLargerValue : fromLessValue)) {
+        result[type] = correctValue;
+        result[handleTypeOpposite] = handleValue.opposite;
+      } else
+      if (isRange && !handleSplit && (isFrom ? toLessValue : fromLargerValue)) {
+        result[type] = handleValue.opposite;
+        result[handleTypeOpposite] = handleValue.current;
+      } else
+      if (!isRange) {
         this.tempData.handleSelected = null;
         result[type] = correctValue;
+        result[handleTypeOpposite] = handleValue.opposite;
       }
     }
     if (action === 'drop') {
@@ -102,98 +109,140 @@ class Presenter {
 
     return result;
   };
-  lineValidate(event: subViewEvent) {
+  lineValidate(event: subViewEvent): SliderStateModified {
     const { to, from, isRange } = this.Model.getState();
     const { action } = event;
-    const value = this.normalizeEventValue(event.value);
+    const value = this.getCorrectPositionFromEventValue(event);
     const result: SliderStateModified = {};
+
+    if (value === false) return {};
+
     if (action === 'click') {
-      if (isRange) {
-        if ((to - from) / 2 + from > value) {
-          result.from = value;
-        } else {
-          result.to = value;
-        }
-      } else {
+      if (isRange && value <= ((to - from) / 2 + from)) {
+        result.from = value;
+      } else
+      if (isRange && value > ((to - from) / 2 + from)) {
+        result.to = value;
+      } else
+      if (!isRange) {
         result.from = value;
       }
     }
     return result;
   }
-  viewEventHandler(event: subViewEvent) {
-    const eventCopy: subViewEvent = {
-      ...event,
-      value: {
-        ...event.value,
-        ...this.getCoordsWithOffSet({
-          x: event.value.x, y: event.value.y,
-        }),
-      },
-    };
+
+  viewEventHandler(event: subViewEvent): void {
     let modelEvent: SliderStateModified = {};
     switch (event.target) {
-      case 'line': modelEvent = this.lineValidate(eventCopy); break;
-      case 'range': modelEvent = this.lineValidate(eventCopy); break;
-      case 'scale': modelEvent = this.lineValidate(eventCopy); break;
-      case 'handle': modelEvent = this.handleValidate(eventCopy); break;
-      case 'tip': modelEvent = this.handleValidate(eventCopy); break;
-
-      default: break;
+      case 'line': modelEvent = this.lineValidate(event); break;
+      case 'range': modelEvent = this.lineValidate(event); break;
+      case 'scale': modelEvent = this.lineValidate(event); break;
+      case 'handle': modelEvent = this.handleValidate(event); break;
+      case 'tip': modelEvent = this.handleValidate(event); break;
     }
     this.updateModel(modelEvent);
   }
-  modelEventHandler(state: SliderState) {
+  modelEventHandler(state: SliderState): void {
     this.updateView(state);
   }
-  updateView(state: SliderState) {
+  updateView(state: SliderState): void {
     this.View.update(this.convertStateToViewState(state));
   }
-  public updateModel(event: SliderStateModified) {
+  public updateModel(event: SliderStateModified): void {
     this.Model.setState(event);
   }
+
   convertStateToViewState(state: SliderState): viewSliderState {
     const { from, to, max, min, horizontal, scaleStep } = state;
     const { height, width } = this.View.getSize();
     return ({
       ...state,
       from: {
-        percent: valueToPercent(from, max, min),
-        px: valueToPixel(from, horizontal ? width : height, max, min),
+        percent: valueToPercent({
+          max: max,
+          min: min,
+          value: from,
+        }),
+        px: valueToPixel({
+          min: min,
+          max: max,
+          size: horizontal ? width : height,
+          value: from
+        }),
         total: from,
         movingNow: this.tempData.handleMovingNow === 'from',
         pressedNow: this.tempData.handlePressedNow === 'from',
         pressedLast: this.tempData.handlePressedLast === 'from',
       },
       to: {
-        percent: valueToPercent(to, max, min),
-        px: valueToPixel(to, horizontal ? width : height, max, min),
+        percent: valueToPercent({
+          max: max,
+          min: min,
+          value: to,
+        }),
+        px: valueToPixel({
+          min: min,
+          max: max,
+          size: horizontal ? width : height,
+          value: to
+        }),
         total: to,
         movingNow: this.tempData.handleMovingNow === 'to',
         pressedNow: this.tempData.handlePressedNow === 'to',
         pressedLast: this.tempData.handlePressedLast === 'to',
       },
-      scaleItemsArray: getScaleItemsArray(max, min, scaleStep),
+      scaleItemsArray: getScaleItemsArray({
+        max: max,
+        min: min,
+        step: scaleStep,
+      }),
     });
   }
-  normalizeEventValue(eventValue: subViewValue): number {
+  getCorrectPositionFromEventValue(event: subViewEvent): number | false {
+    let result: number = 0;
     const { horizontal, min, max, invert } = this.Model.getState();
     const { height, width } = this.View.getSize();
-    const { total, y, x} = eventValue;
-    let result = 0;
-    if (typeof total === 'number') {
-      result = total;
+    const { x, y, total } = event.value;
+
+    if (total === undefined && x === undefined && y === undefined) { return false }
+
+    const correctX = this.getCoordsWithOffSet((x || 0), 'x');
+    const correctY = this.getCoordsWithOffSet((y || 0), 'y');
+    const correctSize = horizontal ? width : height;
+    const correctCoords = horizontal ? correctX : correctY;
+    const correctTotal = total;
+
+
+    if (total !== undefined && typeof correctTotal === 'number') {
+      result = correctTotal;
     } else
-    if (horizontal) {
-      result = invert
-        ? invertValue(max, min, pixelToValue(x, horizontal ? width : height, max, min))
-        : pixelToValue(x, horizontal ? width : height, max, min);
+    if (!invert) {
+      result = pixelToValue({
+        value: correctCoords,
+        size: correctSize,
+        max: max,
+        min: min,
+      })
     } else
-    if (!horizontal) {
-      result = invert
-        ? invertValue(max, min, pixelToValue(y, horizontal ? width : height, max, min))
-        : pixelToValue(y, horizontal ? width : height, max, min);
+    if (invert) {
+      result = pixelToValue({
+        value: correctCoords,
+        size: correctSize,
+        max: max,
+        min: min,
+      })
+      result = invertValue({
+        max: max,
+        min: min,
+        value: result,
+      });
     }
+
     return result;
+  }
+  getCoordsWithOffSet(coords: number, type: 'x' | 'y'): number {
+    const { clientOffSetX, clientOffSetY } = this.View.getSize();
+    return coords - (type === 'x' ? clientOffSetX : clientOffSetY);
   }
 }
 
