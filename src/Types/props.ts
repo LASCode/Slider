a400cccfb509d3763a0d8269956510620b2f0d57
LASCode <geimixa123@gmail.com>
2022-05-15 14:07:45 +0300

    add props types to all utils methods

--- src/Types/props.ts
+++ src/Types/props.ts
@@ -1,14 +1,84 @@
-import { SliderState } from './state';
+import { SliderState, SliderStateModified } from './state';
 import { viewCallbackFunction } from './ViewEventTypes';
 
+interface presenterProps {
+  rootNode: HTMLElement,
+  initialState: SliderStateModified,
+}
 interface viewProps {
  rootNode: HTMLElement,
  callback: viewCallbackFunction
 }
 interface modelProps {
-  state: SliderState,
-  callback: viewCallbackFunction
+  state: SliderStateModified,
+  callback: Function
+}
+interface valueToPercentProps {
+  max: number,
+  min: number,
+  value: number,
+}
+interface valueToPixelProps {
+  size: number,
+  max: number,
+  min: number,
+  value: number,
+}
+interface pixelToPercentProps {
+  value: number,
+  size: number,
+}
+interface pixelToValueProps {
+  value: number,
+  size: number,
+  max: number,
+  min: number,
+}
+interface percentToValueProps {
+  value: number,
+  max: number,
+  min: number,
+}
+interface toFixedProps {
+  value: number,
+  fixed: number,
+}
+interface getNumAfterCommaProps {
+  value: number,
+}
+interface getFixedValueWithStepProps {
+  value: number,
+  step: number,
+}
+interface getScaleItemsArrayProps {
+  max: number,
+  min: number,
+  step: number,
+}
+interface invertValueProps {
+  max: number,
+  min: number,
+  value: number,
+}
+interface checkStepProps {
+  value: number,
+  step: number,
 }
 
-export { viewProps, modelProps };
+export {
+  presenterProps,
+  viewProps,
+  modelProps,
+  valueToPixelProps,
+  valueToPercentProps,
+  pixelToPercentProps,
+  percentToValueProps,
+  pixelToValueProps,
+  toFixedProps,
+  getNumAfterCommaProps,
+  getFixedValueWithStepProps,
+  getScaleItemsArrayProps,
+  invertValueProps,
+  checkStepProps,
+};
 
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,31 +1,72 @@
-const valueToPercent = (value: number, max: number, min: number): number => {
+import {
+  checkStepProps,
+  getFixedValueWithStepProps,
+  getNumAfterCommaProps, getScaleItemsArrayProps, invertValueProps,
+  percentToValueProps,
+  pixelToPercentProps,
+  pixelToValueProps, toFixedProps,
+  valueToPercentProps,
+  valueToPixelProps
+} from '../Types/props';
+
+const valueToPercent = (props: valueToPercentProps): number => {
+  const { value, max, min } = props;
   return (100 / (max - min)) * (value - min);
 };
-const valueToPixel = (value: number, size: number, max: number, min: number): number => {
+const valueToPixel = (props: valueToPixelProps): number => {
+  const { size, max, min, value } = props;
   return (size / 100) * ((100 / (max - min)) * (value - min));
 };
-const percentToValue = (value: number, max: number, min: number): number => {
-  return (((max - min) / 100) * value) + min;
-};
-const pixelToPercent = (value: number, size: number): number => {
+const pixelToPercent = (props: pixelToPercentProps): number => {
+  const { size, value } = props;
   return (100 / size) * value;
 };
-const pixelToValue = (value: number, width: number, max: number, min: number): number => {
-  return percentToValue(pixelToPercent(value, width), max, min);
+const pixelToValue = (props: pixelToValueProps): number => {
+  const { value, size, max, min } = props;
+  const percent = pixelToPercent({
+    value: value,
+    size: size,
+  })
+  return percentToValue({
+    value: percent,
+    max: max,
+    min: min,
+  });
 };
-const toFixed = (value: number, fixed: number): number => {
-  return Number(value.toFixed(fixed));
+const percentToValue = (props: percentToValueProps): number => {
+  const { value, max, min, } = props;
+  return (((max - min) / 100) * value) + min;
 };
-const getNumAfterComma = (value: number): number => {
+const toFixed = (props: toFixedProps): number => {
+  const { value, fixed } = props;
+  let result: number = value;
+  const re = new RegExp('^-?\\d+(?:\.\\d{0,' + (fixed || -1) + '})?');
+  const a = value.toString().match(re);
+  if (a !== null) {
+    result = Number(a[0]);
+  }
+  return result;
+  // return Number(value.toFixed(fixed));
+};
+const getNumAfterComma = (props: getNumAfterCommaProps): number => {
+  const { value } = props;
   if (Math.floor(value) !== value) {
     return value.toString().split('.')[1].length || 0;
   }
   return 0;
 };
-const getFixedValueWithStep = (value: number, step: number): number => {
-  return toFixed(value, getNumAfterComma(step));
+const getFixedValueWithStep = (props: getFixedValueWithStepProps): number => {
+  const { value, step } = props;
+  const numberCount = getNumAfterComma({
+    value: step
+  })
+  return toFixed({
+    value: value,
+    fixed: numberCount,
+  });
 };
-const getScaleItemsArray = (max: number, min: number, step: number): number[] => {
+const getScaleItemsArray = (props: getScaleItemsArrayProps): number[] => {
+  const { max, min, step } = props;
   const result: number[] = [];
   if (step > 0 && max !== min) {
     for (let i = (min < 0 ? 0 : min); i < max; i = i + step) {
@@ -37,12 +78,17 @@ const getScaleItemsArray = (max: number, min: number, step: number): number[] =>
     result.push(max);
   }
   if (min < 0) result.unshift(min);
-  return result.map((el) => getFixedValueWithStep(el, step));;
+  return result.map((el) => getFixedValueWithStep({
+    value: el,
+    step: step,
+  }));
 };
-const invertValue = (max: number, min: number, value: number): number => {
+const invertValue = (props: invertValueProps): number => {
+  const { max, min, value } = props;
   return max - value + min;
 };
-const checkStep = (value: number, step: number) => {
+const checkStep = (props: checkStepProps) => {
+  const { value, step } = props;
   const stepCopy = (step <= 0 ? 1 : step);
   return ((Math.round(value / stepCopy) * 100) / 100) * stepCopy;
 };
@@ -52,10 +98,12 @@ export {
   valueToPercent,
   valueToPixel,
   pixelToValue,
+  percentToValue,
   getScaleItemsArray,
   pixelToPercent,
   invertValue,
   getFixedValueWithStep,
   getNumAfterComma,
   checkStep,
+  toFixed,
 };
